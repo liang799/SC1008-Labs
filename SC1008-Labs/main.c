e6206260b0872290feda3f95e37f88bf9a47fc63
liang799 <71926348+liang799@users.noreply.github.com>
2026-01-23 13:38:37 +0800

    Initial Commit

---
+++ SC1008-Labs/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  SC1008-Labs
+//
+//  Created by Neoh Tian Pok on 23/1/26.
+//
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return EXIT_SUCCESS;
+}
