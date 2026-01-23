4f738494495789af1b059baa677c56059c0b51e9
liang799 <71926348+liang799@users.noreply.github.com>
2026-01-23 14:32:32 +0800

    solved question 1

--- SC1008-Labs/Lab 1/main.c
+++ SC1008-Labs/Lab 1/main.c
@@ -8,8 +8,39 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
-    return EXIT_SUCCESS;
+char determineGrade(int mark);
+int studentId;
+int studentMark;
+
+int main() {
+
+    while(1) {
+        printf("Enter Student ID:\n");
+        scanf("%d", &studentId);
+
+        if (studentId == -1) {
+            return 0;
+        }
+
+        printf("Enter Mark:\n");
+        scanf("%d", &studentMark);
+
+        printf("Grade = %c\n", determineGrade(studentMark));
+    }
+
+    return 0;
+}
+
+char determineGrade(int mark) {
+    if (mark >= 75) {
+        return 'A';
+    } else if (mark >= 65) {
+        return 'B';
+    } else if (mark >= 55) {
+        return 'C';
+    } else if (mark >= 45) {
+        return 'D';
+    } else {
+        return 'F';
+    }
 }
