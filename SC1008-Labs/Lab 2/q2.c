413ef32b2daac3e2a153a40409b3537bd3479f08
liang799 <71926348+liang799@users.noreply.github.com>
2026-01-29 13:40:31 +0800

    feat: completed lab2 q2

---
+++ SC1008-Labs/Lab 2/q2.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+int digitPos1(int num, int digit);
+void digitPos2(int num, int digit, int *result);
+
+int main() {
+    int number, digit, result = 0;
+
+    printf("Enter the number: \n");
+    scanf("%d", &number);
+
+    printf("Enter the digit: \n");
+    scanf("%d", &digit);
+
+    printf("digitPos1(): %d\n", digitPos1(number, digit));
+    digitPos2(number, digit, &result);
+    printf("digitPos2(): %d\n", result);
+
+    return 0;
+}
+
+int digitPos1(int num, int digit) {
+    int result = 0;
+    do {
+        if (num % 10 == digit) {
+            return result;
+        }
+        num = num / 10;
+    } while (num > 0);
+
+    return result;
+}
+
+void digitPos2(int num, int digit, int *result) {
+    int pos = 0;
+    *result = 0;
+    do {
+        pos++;
+        if (num % 10 == digit) {
+            *result = pos;
+            break;
+        }
+        num = num / 10;
+    } while (num > 0);
+}
