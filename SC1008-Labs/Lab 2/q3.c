e67b904c73e9a773585c9fe3640729d1491cecdb
liang799 <71926348+liang799@users.noreply.github.com>
2026-01-31 15:16:56 +0800

    refactor: made code APAS compliant

--- SC1008-Labs/Lab 2/q3.c
+++ SC1008-Labs/Lab 2/q3.c
@@ -16,23 +16,27 @@ int main() {
 }
 
 int square1(int num) {
+    int sum = 0;
+    int i = 0;
+
     if (num == 0) {
         return 0;
     }
-    int sum = 0;
-    for (int i = 1; sum / num != num; i += 2) {
+    for (i = 1; sum / num != num; i += 2) {
         sum += i;
     }
     return sum;
 }
 
 void square2(int num, int *result) {
+    int i = 0;
+
     if (num == 0) {
         *result = 0;
         return;
     }
     *result = 0;
-    for (int i = 1; *result / num != num; i += 2) {
+    for (i = 1; *result / num != num; i += 2) {
         *result += i;
     }
 }
