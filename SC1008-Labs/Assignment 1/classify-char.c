b5da3f650d2bb571ff2fd5b67292dfc90d7ef830
liang799 <71926348+liang799@users.noreply.github.com>
2026-01-31 10:25:17 +0800

    feat: finish assignment 1, classifying char question

---
+++ SC1008-Labs/Assignment 1/classify-char.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+char input;
+
+int main() {
+    /* Write your code here */
+    printf("Enter a character:\n");
+    scanf("%c", &input);
+
+    if (input >= 'a' && input <= 'z') {
+        printf("Lower case letter");
+    } else if (input >= 'A' && input <= 'Z') {
+        printf("Upper case letter");
+    } else {
+        printf("Other character");
+    }
+
+    return 0;
+}
