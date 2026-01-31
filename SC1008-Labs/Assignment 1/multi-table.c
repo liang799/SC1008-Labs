db8c248d372e194d138ccd758492ea71ed22c6cf
liang799 <71926348+liang799@users.noreply.github.com>
2026-01-31 15:34:29 +0800

    refactor: made code apas compliant

--- SC1008-Labs/Assignment 1/multi-table.c
+++ SC1008-Labs/Assignment 1/multi-table.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 
-int input;
-
 int main() {
     /* Write your code here */
+    int input, i, j, k;
+
     printf("Enter a number (between 1 and 9):\n");
     scanf("%d", &input);
 
     printf("Multiplication Table:\n");
-    for (int k = 0; k < input + 1; k++) {
+    for (k = 0; k < input + 1; k++) {
         if (k == 0) {
             printf("  ");
         } else {
@@ -18,9 +18,9 @@ int main() {
 
     printf("\n");
 
-    for (int i = 1; i < input + 1; i++) {
+    for (i = 1; i < input + 1; i++) {
         printf("%d ", i);
-        for (int j = 1; j < i + 1; j++) {
+        for (j = 1; j < i + 1; j++) {
             printf("%d ", i * j);
         }
         printf("\n");
