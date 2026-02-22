353a07802ce60b6a7c607678b6170f4da70bfabf
liang799 <71926348+liang799@users.noreply.github.com>
2026-02-22 23:35:14 +0800

    feat: complete lab3 reverse 1D array

---
+++ SC1008-Labs/Lab 3/reverseAr1D.c
@@ -0,0 +1,37 @@
+/*edit*/
+
+/*custom header*/
+
+/*end_edit*/
+#include <stdio.h>
+void reverseAr1D(int ar[], int size);
+int main() {
+    int ar[10];
+    int size, i;
+
+    printf("Enter array size: \n");
+    scanf("%d", &size);
+    printf("Enter %d data: \n", size);
+    for (i = 0; i <= size - 1; i++)
+        scanf("%d", &ar[i]);
+    reverseAr1D(ar, size);
+    printf("reverseAr1D(): ");
+    if (size > 0) {
+        for (i = 0; i < size; i++)
+            printf("%d ", ar[i]);
+    }
+    return 0;
+}
+void reverseAr1D(int ar[], int size) {
+    /*edit*/
+    /* Write your code here */
+    int i, tmp;
+
+    for (i = 0; i < size / 2; i++) {
+        tmp = ar[i];
+        ar[i] = ar[size - i - 1];
+        ar[size - i - 1] = tmp;
+    }
+
+    /*end_edit*/
+}
